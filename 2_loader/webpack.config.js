5f8ed2e0c2873bc8e258ce7d6a9142f04df2d99c
n00nietzsche <n00nietzsche@gmail.com>
2021-09-18 08:41:53 +0900

    로더 실습 풀이 완료

--- 2_loader/webpack.config.js
+++ 2_loader/webpack.config.js
@@ -1,12 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/app.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    mode: "development",
+    entry: {
+        main: "./src/app.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve("./dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    // 실행 순서는 css-loader부터 실행됨
+                    "style-loader",
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|svg)/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 4 * 1024,
+                    },
+                },
+            },
+        ],
+    },
+};
