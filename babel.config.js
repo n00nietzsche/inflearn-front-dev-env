f806875e911e954081782e85044d234368186b5b
n00nietzsche <n00nietzsche@gmail.com>
2021-09-20 01:54:59 +0900

    프론트엔드 개발 환경 공부 #9 바벨의 기본 개념 (플러그인, 프리셋)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-    presets: ["./custom-babel-preset.js"],
-    // plugins: [
-    //     "@babel/plugin-transform-block-scoping",
-    //     "@babel/plugin-transform-arrow-functions",
-    //     "@babel/plugin-transform-strict-mode",
-    // ],
+    presets: ["@babel/preset-env"],
 };
