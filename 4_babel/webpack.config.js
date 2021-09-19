bf4d54662df8366e50245d597c55121a6524ef70
n00nietzsche <n00nietzsche@gmail.com>
2021-09-20 04:15:44 +0900

    프론트엔드 개발 환경 공부 #11 최신 문법으로 작성한 프로젝트를 IE에서 동작하게 만들어보기

--- 4_babel/webpack.config.js
+++ 4_babel/webpack.config.js
@@ -1,66 +1,62 @@
 const path = require("path");
 const webpack = require("webpack");
-
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 module.exports = {
-  mode: "development",
-  entry: {
-    main: "./src/app.js"
-  },
-  output: {
-    filename: "[name].js",
-    path: path.resolve("./dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          process.env.NODE_ENV === "production"
-            ? MiniCssExtractPlugin.loader // 프로덕션 환경
-            : "style-loader", // 개발 환경
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.(png|jpg|svg|gif)$/,
-        loader: "url-loader",
-        options: {
-          name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
-      /**
-       * TODO: babel-loader를 구성해 보세요.
-       */
-    ]
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: `빌드 날짜: ${new Date().toLocaleString()}`
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
-      },
-      minify:
-        process.env.NODE_ENV === "production"
-          ? {
-              collapseWhitespace: true, // 빈칸 제거
-              removeComments: true // 주석 제거
-            }
-          : false,
-      hash: process.env.NODE_ENV === "production"
-    }),
-    new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === "production"
-      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : [])
-  ]
+    mode: "development",
+    entry: {
+        main: "./src/app.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve("./dist"),
+        // environment: {
+        //     arrowFunction: false,
+        //     destructuring: false,
+        //     dynamicImport: false,
+        //     forOf: false,
+        //     module: false,
+        // },
+        publicPath: "./",
+    },
+    target: ["web", "es5"],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                type: "asset",
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    /**
+     * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
+     * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
+     * 2. HtmlWebpackPlugin: 동적으로 html 파일을 생성하세요.
+     * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
+     * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
+     */ plugins: [
+        new webpack.BannerPlugin({
+            banner: `빌드 시간 ${new Date().toLocaleString()}`,
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            templateParameters: {
+                env:
+                    process.env.NODE_ENV === "production" ? "프로덕션" : "개발",
+            },
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+    ],
 };
