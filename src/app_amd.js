57f3548f199253493f2fb833d69f42b737798bdc
n00nietzsche <n00nietzsche@gmail.com>
2021-09-19 10:51:23 +0900

    프론트엔드 개발 환경 공부 #8 자주 사용하는 플러그인

--- src/app_amd.js
+++ src/app_amd.js
@@ -6,3 +6,7 @@ import superSmallImage from "./super_small_image.png";
 console.log("1+2 = ", math.sum(1, 2));
 console.log("image", image);
 console.log("superSmallImage", superSmallImage);
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+console.log("DEFINE_PLUGIN_TEST", DEFINE_PLUGIN_TEST);
+console.log("DEFINE_PLUGIN_STRING", DEFINE_PLUGIN_STRING);
+console.log("SERVICE_URL", SERVICE_URL);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 // node.js의 CommonJS 를 사용한 모듈화
 const path = require("path");
 const HelloWorldPlugin = require("./custom-plugin");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
@@ -20,6 +25,8 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
+                    process.env.NODE_ENV === "production" ?
+                    MiniCssExtractPlugin.loader :
                     // 로더의 실행 순서는 뒤에서부터 앞이다.
                     "style-loader",
                     "css-loader",
@@ -36,5 +43,42 @@ module.exports = {
             },
         ],
     },
-    plugins: [new HelloWorldPlugin({ options: true })],
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: `
+            name: [name] 
+            content: 배너 테스트 
+            date: ${new Date().toLocaleDateString()}
+            commit version: ${childProcess.execSync(
+                "git rev-parse --short HEAD"
+            )}
+            author: ${childProcess.execSync("git config user.name")} 
+            `,
+        }),
+        new webpack.DefinePlugin({
+            DEFINE_PLUGIN_TEST: "1+1",
+            DEFINE_PLUGIN_STRING: JSON.stringify("1+1"),
+            SERVICE_URL: JSON.stringify("https://myservice.com/"),
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            templateParameters: {
+                env:
+                    process.env.NODE_ENV === "development"
+                        ? "(개발용)"
+                        : "(프로덕션용)",
+            },
+            minify:
+                process.env.NODE_ENV === "production"
+                    ? {
+                          collapseWhitespace: true,
+                          removeComments: true,
+                      }
+                    : false,
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
 };
