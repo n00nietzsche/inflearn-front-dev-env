c48f64ba9515b4037cb1d2a5424234f67b4266fd
n00nietzsche <n00nietzsche@gmail.com>
2021-09-20 01:45:40 +0900

    프론트엔드 개발 환경 공부 #9 바벨의 기본 개념

---
+++ custom-babel-preset.js
@@ -0,0 +1,9 @@
+module.exports = function customBabelPreset() {
+    return {
+        plugins: [
+            "@babel/plugin-transform-block-scoping",
+            "@babel/plugin-transform-arrow-functions",
+            "@babel/plugin-transform-strict-mode",
+        ],
+    };
+};
